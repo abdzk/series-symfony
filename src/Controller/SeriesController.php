2c0b7c0b4d523453dbf625fefa6ebcec0ae50c32
abdzk <aurore.budzik@gmail.com>
2022-08-29 12:58:12 +0200

    creation les pages de base pour les series

---
+++ src/Controller/SeriesController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SeriesController extends AbstractController
+{
+    #[Route('/series', name: 'serie_list')]
+    public function list(): Response
+    {
+        //todo:aller chercher les series en BDD
+        return $this->render('series/list.html.twig', [
+
+        ]);
+    }
+
+    #[Route('/series/details/{id}', name: 'serie_details')]
+    public function details(int $id): Response
+    {
+        //todo:aller chercher les series en BDD
+        return $this->render('series/details.html.twig', [
+
+        ]);
+    }
+
+    #[Route('/series/create', name: 'serie_create')]
+    public function create(): Response
+    {
+        //todo:aller chercher les series en BDD
+        return $this->render('series/create.html.twig', [
+
+        ]);
+    }
+}
